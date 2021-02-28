412565289893cf17adfd13f11dcced0b7597cfa4
Change_Huang <928219190@qq.com>
2021-02-28 20:16:13 +0800

    基本完成登录界面绘制

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import './assets/css/iconfont/iconfont.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1/'
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
---
+++ src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from './components/login/login.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [{
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  }]
+})
+
+export default router
